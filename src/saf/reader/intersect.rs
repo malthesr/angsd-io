5ea44dac7b6167fdf4f2e9b78a75cecd8241cab9
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-09-01 14:51:06 +0200

    saf/reader/intersect: Docs, remove V3 default, small optimisation

--- src/saf/reader/intersect.rs
+++ src/saf/reader/intersect.rs
@@ -5,7 +5,7 @@ use indexmap::IndexMap;
 use crate::{
     saf::{
         record::{Id, Record},
-        Version, V3,
+        Version,
     },
     ReadStatus,
 };
@@ -14,11 +14,13 @@ use super::{BgzfReader, Index};
 
 /// An intersection of BGZF SAF file readers.
 ///
-/// Created by the [`BgzfReader::intersect`] method.
-pub struct Intersect<R, V: Version = V3> {
+/// The intersection takes an arbitrary number of readers and returns data where all readers
+/// contain data for the same contig and position. It is assumed that contigs are sorted in the
+/// same order in each file, and that positions are sorted numerically within each contig.
+pub struct Intersect<R, V> {
     readers: Vec<BgzfReader<R, V>>,
-    contigs: Contigs,
-    ids: Vec<usize>,
+    shared_contigs: SharedContigs,
+    ids: Vec<usize>, // Current reader contig IDs
 }
 
 impl<R, V> Intersect<R, V>
@@ -27,10 +29,6 @@ where
     V: Version,
 {
     /// Returns a new collection of records suitable for use in reading.
-    ///
-    /// The [`Self::read_records`] method requires a collection of record buffers of the correct
-    /// length and with the correct number of alleles. This method creates such a record collection,
-    /// using the number of alleles defined in the indexes.
     pub fn create_record_bufs(&self) -> Vec<Record<Id, V::Item>> {
         self.readers
             .iter()
@@ -44,7 +42,7 @@ where
     /// of multiple readers. See also the [`BgzfReader::intersect`] method for a way to start create
     /// the initial intersecting reader.
     pub fn intersect(mut self, reader: BgzfReader<R, V>) -> Self {
-        self.contigs.add_index(reader.index());
+        self.shared_contigs.add_index(reader.index());
         self.readers.push(reader);
         self.ids.push(0);
         self
@@ -74,7 +72,7 @@ where
         match readers.as_slice() {
             [] => panic!("cannot construct empty intersection"),
             [fst, tl @ ..] => {
-                let mut contigs = Contigs::from(fst.index());
+                let mut contigs = SharedContigs::from(fst.index());
                 for reader in tl.iter() {
                     contigs.add_index(reader.index());
                 }
@@ -83,7 +81,7 @@ where
 
                 Self {
                     readers,
-                    contigs,
+                    shared_contigs: contigs,
                     ids,
                 }
             }
@@ -95,9 +93,8 @@ where
     /// If successful, a record from each inner reader will be read into the corresponding buffer
     /// such that all resulting records will be on the same contig and the same position.
     ///
-    /// Note that the number of provided buffers and their contents must match the inner readers
-    /// and their contents, respectively. See [`Self::create_record_bufs`] to create an appropriate
-    /// collection of buffers based on the reader indices.
+    /// Note that the record buffer needs to be correctly set up. Use [`Self::create_record_bufs`]
+    /// for a correctly initialised record buffers to use for reading.
     pub fn read_records(&mut self, bufs: &mut [Record<Id, V::Item>]) -> io::Result<ReadStatus> {
         for ((reader, record), id) in self
             .readers
@@ -123,21 +120,32 @@ where
         }
     }
 
-    pub(crate) fn from_reader(reader: BgzfReader<R, V>) -> Self {
+    pub(super) fn from_reader(reader: BgzfReader<R, V>) -> Self {
         Self {
-            contigs: Contigs::from(reader.index()),
+            shared_contigs: SharedContigs::from(reader.index()),
             readers: vec![reader],
             ids: vec![0],
         }
     }
 
+    /// Read all readers until they are on a shared contig equal to or after the contigs defined
+    /// by the provided record buffers.
+    ///
+    /// If no more shared contigs exist, returns `Done`.
     fn read_until_shared_contig(
         &mut self,
         bufs: &mut [Record<Id, V::Item>],
     ) -> io::Result<ReadStatus> {
+        // For each record, get the first shared contig (by index into shared_contigs)
+        // equal to or after the contig ID of that record. Then get the greatest/most distant of
+        // those shared contigs. We can safely seek to this contig. If in the process, we find
+        // that a reader has no such shared contigs, we are done.
         let mut next_idx = 0;
         for (reader, buf) in self.readers.iter_mut().zip(bufs.iter_mut()) {
-            match self.contigs.next_shared(*buf.contig_id(), reader.index()) {
+            match self
+                .shared_contigs
+                .next_shared(reader.index(), *buf.contig_id())
+            {
                 Some(idx) => {
                     if idx > next_idx {
                         next_idx = idx;
@@ -147,7 +155,8 @@ where
             }
         }
 
-        let next_ids = &self.contigs[next_idx];
+        // Seek all readers to candidate shared contig, if they are not on it already.
+        let next_ids = &self.shared_contigs.0[next_idx];
         for (((reader, buf), next_id), id) in self
             .readers
             .iter_mut()
@@ -165,6 +174,14 @@ where
         Ok(ReadStatus::NotDone)
     }
 
+    /// Reads all readers until they are on a shared position on the current contig.
+    ///
+    /// The starting read positions will be defined by the provided buffers; the current contig is
+    /// defined by `self.ids`.
+    ///
+    /// If no more shared positions exist, returns `Some(Done)`. If more shared positions may exist,
+    /// but not on the current contig, returns `None`. If `Some(NotDone)` is returned, an
+    /// intersecting positions has been found.
     fn read_until_shared_position_on_contig(
         &mut self,
         bufs: &mut [Record<Id, V::Item>],
@@ -176,7 +193,10 @@ where
             .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "empty buffer slice"))?;
 
         'outer: loop {
-            for ((reader, record), id) in self
+            // We keep checking if all the records have reached the max position:
+            // if so, we have a shared record. If we find one greater than max, max is updated.
+            // As we go, we have to check that we have not reached a new contig ID.
+            'inner: for ((reader, record), id) in self
                 .readers
                 .iter_mut()
                 .zip(bufs.iter_mut())
@@ -184,23 +204,30 @@ where
             {
                 let mut pos = record.position();
 
+                // A shared position must be at least as great as the max among all records
                 match pos.cmp(&max_pos) {
                     Ordering::Less => {
+                        // If a position is less than the current max, we can forward the
+                        // corresponding reader all the way to its first position equal to or
+                        // greater than the current max
                         while pos < max_pos {
                             if reader.read_record(record)?.is_done() {
                                 return Ok(Some(ReadStatus::Done));
                             }
-
                             if record.contig_id() != id {
                                 *id = *record.contig_id();
-
                                 return Ok(None);
                             }
-
                             pos = record.position();
                         }
 
-                        continue 'outer;
+                        if pos == max_pos {
+                            continue 'inner;
+                        } else {
+                            // Forward overshot current max_pos, which means we have to start over
+                            // checking all records
+                            continue 'outer;
+                        }
                     }
                     Ordering::Equal => (),
                     Ordering::Greater => {
@@ -210,21 +237,37 @@ where
                 }
             }
 
+            // To have reached this point, all record positions are at the current max, which means
+            // an intersection
             return Ok(Some(ReadStatus::NotDone));
         }
     }
 }
 
+/// Shared contigs for readers based on their indexes.
+///
+/// The representation used is an ordered map from contig names to a vector of contig IDs,
+/// corresponding to the contig IDs used in the represented reader. For instance, if "chr1"
+/// is the first shared contig among two represented reads, and the first entry in the map is
+/// ("chr1", vec![1, 2]), then "chr1" has ID 1 in the first reader, and ID 2 in the second reader.
+/// As elsewhere here, the ID is based on the position in the index.
+///
+/// Note that as for `Intersect` generally, we assume that contigs occur in the same order in each
+/// index. That is, the same contigs may not be represented in each index, and the same contig may
+/// have a different IDs, but where two or more contigs occur in multiple indices, their ordering
+/// must be constant.
 #[derive(Clone, Debug)]
-struct Contigs {
-    contigs: IndexMap<String, Vec<usize>>,
-}
+struct SharedContigs(IndexMap<String, Vec<usize>>);
 
-impl Contigs {
-    fn add_index<V>(&mut self, index: &Index<V>)
+impl SharedContigs {
+    /// Adds a new index to the collection.
+    ///
+    /// The new index will be placed last.
+    pub fn add_index<V>(&mut self, index: &Index<V>)
     where
         V: Version,
     {
+        // Create a temporary mapping from names to IDs in the new index
         let map: IndexMap<&str, usize> = index
             .records()
             .iter()
@@ -232,31 +275,40 @@ impl Contigs {
             .map(|(i, record)| (record.name(), i))
             .collect();
 
-        self.contigs.retain(|name, ids| {
+        // For each name in the current selection, check if the name exists in the new index:
+        // if so, (1) add its ID in the new index to the collection of IDs for this shared contig;
+        // if not, (2) the contig is no longer shared, and should be removed
+        self.0.retain(|name, ids| {
             if let Some(new_id) = map.get(name.as_str()) {
+                // (1)
                 ids.push(*new_id);
                 true
             } else {
+                // (2)
                 false
             }
         })
     }
 
-    fn next_shared<V>(&self, id: usize, index: &Index<V>) -> Option<usize>
+    /// Returns the index in `self` of the first contig in `index` with ID equal to or greater
+    /// than `id`.
+    ///
+    /// If no more shared contigs exist in `index` after `id`, return `None`.
+    pub fn next_shared<V>(&self, index: &Index<V>, id: usize) -> Option<usize>
     where
         V: Version,
     {
         let name = index.records()[id].name();
 
-        self.contigs.get_index_of(name).or_else(|| {
+        self.0.get_index_of(name).or_else(|| {
             index.records()[(id + 1)..]
                 .iter()
-                .find_map(|record| self.contigs.get_index_of(record.name()))
+                .find_map(|record| self.0.get_index_of(record.name()))
         })
     }
 }
 
-impl<V> From<&Index<V>> for Contigs
+impl<V> From<&Index<V>> for SharedContigs
 where
     V: Version,
 {
@@ -270,23 +322,12 @@ where
     }
 }
 
-impl FromIterator<(String, Vec<usize>)> for Contigs {
+impl FromIterator<(String, Vec<usize>)> for SharedContigs {
     fn from_iter<I>(iter: I) -> Self
     where
         I: IntoIterator<Item = (String, Vec<usize>)>,
     {
-        let contigs = iter.into_iter().map(|(s, a)| (s, a)).collect();
-
-        Self { contigs }
-    }
-}
-
-impl std::ops::Index<usize> for Contigs {
-    type Output = [usize];
-
-    #[inline]
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.contigs[index]
+        Self(iter.into_iter().map(|(s, a)| (s, a)).collect())
     }
 }
 
@@ -294,7 +335,7 @@ impl std::ops::Index<usize> for Contigs {
 mod tests {
     use super::*;
 
-    use crate::saf::tests::*;
+    use crate::saf::{tests::*, V3};
 
     fn test_intersect<R>(mut intersect: Intersect<R, V3>, shared: &[(&str, u32)]) -> io::Result<()>
     where
@@ -331,6 +372,22 @@ mod tests {
 
         let intersect = left_reader.intersect(right_reader);
         let shared = vec![("chr2", 7), ("chr5", 1), ("chr7", 9)];
+
+        test_intersect(intersect, &shared)
+    }
+
+    #[test]
+    fn test_intersect_two_simple() -> io::Result<()> {
+        let left_reader = reader!(records![
+            "chr1":1, "chr1":2, "chr1":3
+        ]);
+
+        let right_reader = reader!(records![
+            "chr1":1, "chr1":2, "chr1":3
+        ]);
+
+        let intersect = left_reader.intersect(right_reader);
+        let shared = vec![("chr1", 1), ("chr1", 2), ("chr1", 3)];
 
         test_intersect(intersect, &shared)
     }
