a5ffe61f511553fa8ee87775828d6928f272225a
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-09-03 17:43:24 +0200

    saf/examples: Add V4 read example

---
+++ saf/examples/saf_v4_read.rs
@@ -0,0 +1,25 @@
+//! Read a SAF file and print readable index and records.
+
+use std::{
+    env,
+    io::{self, Write},
+};
+
+use angsd_saf as saf;
+
+fn main() -> io::Result<()> {
+    let path = env::args().nth(1).expect("missing path to SAF member file");
+    let mut reader = saf::ReaderV4::from_member_path(path)?;
+
+    let stdout = io::stdout();
+    let mut writer = stdout.lock();
+
+    write!(writer, "{}", reader.index())?;
+
+    let mut record = reader.create_record_buf();
+    while reader.read_record(&mut record)?.is_not_done() {
+        writeln!(writer, "{record:.2}")?;
+    }
+
+    Ok(())
+}
